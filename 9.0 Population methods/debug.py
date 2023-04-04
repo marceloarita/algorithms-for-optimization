2aa81eb4d6b04eb5ab211ff0a0a5a42266728669
Marcelo Arita <marceloarita@Marcelos-MacBook-Air.local>
2023-04-03 22:47:51 -0300

    Added new test function

--- 9.0 Population methods/debug.py
+++ 9.0 Population methods/debug.py
@@ -3,8 +3,14 @@
 
 from population_methods import *
 import matplotlib.pyplot as plt
+random.seed(42)
 
-pm = PopulationMethods()
+ga = GeneticAlgorithm(k=10)
+f = lambda x: np.apply_along_axis(lambda y: np.linalg.norm(y), 1, x)
+m = 1000
+k_max = 10
+population = ga.rand_population_uniform(m,a=[-3,3], b=[3,3])
+
 
 def plot_distribution(pop_data: list, title: str) -> None:
     plt.scatter(pop_data[:,0], pop_data[:,1], s=10)
@@ -12,41 +18,109 @@ def plot_distribution(pop_data: list, title: str) -> None:
     plt.ylim(-4, 4)
     plt.xlabel('X Axis')
     plt.ylabel('Y Axis')
-    plt.title(title)``
+    plt.title(title)
     plt.show()
     return None
 
+plot_distribution(population, title='Uniform distribution')
 
-# Uniform
-m = 1000
-a = [-2, -2]
-b = [2, 2]
-pop_uniform = pm.rand_population_uniform(m, a, b)
-plot_distribution(pop_data=pop_uniform, title='Uniform distribution')
+ga.genetic_algorithm(f=f,select=ga.truncation_selection, crossover=ga.single_point_crossover, mutate=ga.bit_wise_mutation, population=population, k_max=k_max)
 
-# Normal
-mean = [0,0]
-cov = [[1,0],[0,1]]
-pop_normal = pm.rand_population_normal(m, mean, cov)
-plot_distribution(pop_data=pop_normal, title='Normal distribution')
 
-# Cauchy
-scale = [1, 1]
-pop_cauchy = pm.rand_population_cauchy(m, mean, scale)
-plot_distribution(pop_data=pop_cauchy, title='Cauchy distribution')
+for k in range(k_max):
+    parents = ga.truncation_selection(y=f(population))
+    children = [ga.single_point_crossover(parent_1=population[p[0]], parent_2=population[p[1]]) for p in parents]
+    population = [ga.bit_wise_mutation(child, r=0.5) for child in children]
+new_poulation = population[np.argmin(f(population))]
 
-rand_bit_pop = pm.generate_rand_bit_population(m=10)
+
+
+
+
+# pop_uniform = ga.rand_population_uniform(m, a, b)
+# plot_distribution(pop_data=pop_uniform, title='Uniform distribution')
+
+# # Uniform
+# m = 1000
+# a = [-2, -2]
+# b = [2, 2]
+# pop_uniform = ga.rand_population_uniform(m, a, b)
+# plot_distribution(pop_data=pop_uniform, title='Uniform distribution')
+
+# # Normal
+# mean = [0,0]
+# cov = [[1,0],[0,1]]
+# pop_normal = ga.rand_population_normal(m, mean, cov)
+# plot_distribution(pop_data=pop_normal, title='Normal distribution')
+
+# # Cauchy
+# scale = [1, 1]
+# pop_cauchy = ga.rand_population_cauchy(m, mean, scale)
+# plot_distribution(pop_data=pop_cauchy, title='Cauchy distribution')
+
+rand_bit_pop = ga.generate_rand_bit_population(m=10)
 y = [random.randint(0,100) for i in range(10)]
 
-pm.truncation_selection(y=y)
-pm.tournament_selection(y=y)
-pm.roulette_wheel_selection(y=y)
-y = [5, 2, 7, 3, 9, 1, 8, 4, 6]
-def getparent():
-        p = random.sample(range(len(y)), 9)
-        return p[np.argmin([y[i] for i in p])]
-[[getparent(), getparent()] for i in y]
+ga.truncation_selection(y=y)
+ga.tournament_selection(y=y)
+ga.roulette_wheel_selection(y=y)
 
 
+parent_1 = ga.generate_rand_bit_population(m=10)
+parent_2 = ga.generate_rand_bit_population(m=10)
+child = parent_1
+mut_child = [abs(1-v) if np.random.random() < 0.5 else v for v in child]
 
-from numpy.random import standard_cauchy, multivariate_normal, random, randint
+import numpy as np
+
+def michalewicz(x, m=10):
+    res = -np.sum(np.sin(x) * np.power(np.sin(np.arange(1, len(x)+1) * np.power(x, 2) / np.pi), 2*m))
+    return res
+
+x = [2.20, 1.57]
+michalewicz(x)
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objs as go
+from plotly.offline import iplot
+
+# Definir intervalo de valores para x e y
+x = np.linspace(0, np.pi, 50)
+y = np.linspace(0, np.pi, 50)
+
+# Calcular o valor da função para cada ponto na grade
+X, Y = np.meshgrid(x, y)
+Z = np.zeros_like(X)
+for i in range(len(x)):
+    for j in range(len(y)):
+        Z[i,j] = michalewicz([X[i,j], Y[i,j]])
+
+# Criar um traçado 3D
+surface = go.Surface(x=X, y=Y, z=Z, colorscale='Viridis')
+
+# Criar um traçado para o marcador
+point = go.Scatter3d(
+    x=[2.20],
+    y=[1.57],
+    z=[michalewicz([2.20, 1.57])],
+    mode='markers',
+    marker=dict(
+        size=5,
+        color='red'
+    )
+)
+
+# Definir o layout
+layout = go.Layout(
+    scene=dict(
+        xaxis=dict(title='x'),
+        yaxis=dict(title='y'),
+        zaxis=dict(title='michalewicz(x)')
+    )
+)
+
+# Plotar o gráfico iterativo
+fig = go.Figure(data=[surface, point], layout=layout)
+iplot(fig)
+
--- 9.0 Population methods/population_methods.py
+++ 9.0 Population methods/population_methods.py
@@ -13,64 +13,104 @@ class SelectionMethods():
         pass
 
     def truncation_selection(self, y: list) -> list:
-        index = np.argsort(y) 
-        index_pairs = [[index[randint(0, self.k - 1)] for j in range(2)] for i in range(len(y))]
+        index = np.argsort(y)
+        index_pairs = [[index[randint(0, self.k)]
+                        for j in range(2)] for i in y]
         return index_pairs
-    
-    def tournament_selection(self, y: list) -> list:
-        
-        def getparent():
-            p = random.sample(range(len(y)), self.k)
-            index = p[np.argmin([y[i] for i in p])]
-            return index 
-        
-        index_pairs = [[getparent(), getparent()] for i in y]
 
+    def tournament_selection(self, y: list) -> list:
+
+        def getparent(y: list):
+            y = np.array(y)
+            p = np.random.permutation(len(y))
+            index = p[np.argmin(y[p[:self.k]])]
+            return index
+
+        index_pairs = [[getparent(y), getparent(y)] for i in range(len(y))]
         return index_pairs
 
     def roulette_wheel_selection(self, y: list) -> list:
         y = [(max(y) - yi) for yi in y]
         cum_probs = np.cumsum(y)/sum(y)
-        index_pairs = [(np.searchsorted(cum_probs, np.random.random()), np.searchsorted(cum_probs, np.random.random())) for i in y]
+        index_pairs = [[np.searchsorted(cum_probs, np.random.random()), np.searchsorted(
+            cum_probs, np.random.random())] for i in y]
         return index_pairs
 
 
-class PopulationMethods(SelectionMethods):
+class CrossoverMethods():
 
     def __init__(self) -> None:
-        super().__init__(k=10)
         pass
 
-    def rand_population_normal(self: 'PopulationMethods', m: int, mean: list, cov: list) -> np.array:
+    def single_point_crossover(self, parent_1: list, parent_2: list) -> list:
+        i = random.randint(0, len(parent_1))
+        new_one = np.concatenate((parent_1[:i], parent_2[i:]))
+        return new_one
+
+    def two_point_crossover(self, parent_1: list, parent_2: list) -> list:
+        n = len(parent_1)
+        i, j = sorted(random.sample(range(n), 2))
+        new_one = parent_1[:i] + parent_2[i:j] + parent_1[j:]
+        return new_one
+
+    def uniform_crossover(self, parent_1: list, parent_2: list) -> list:
+        new_one = [parent_2[i] if random.random() < 0.5 else parent_1[i]
+                   for i in range(len(parent_1))]
+        return new_one
+
+
+class MutationMethods():
+
+    def __init__(self) -> None:
+        pass
+
+    def bit_wise_mutation(self, child: list, r: float):
+        mut_child = [abs(1-v) if np.random.random() <
+                     r else v for v in child]
+        return mut_child
+
+    # def gaussian_mutation(self, child: list, sigma: float):
+    #     mut_child = child + np.random.randn(len(child)) * sigma
+    #     return mut_child
+
+
+class GeneticAlgorithm(SelectionMethods, CrossoverMethods, MutationMethods):
+
+    def __init__(self, k: int) -> None:
+        super().__init__(k=k)
+        pass
+
+    def rand_population_normal(self: 'GeneticAlgorithm', m: int, mean: list, cov: list) -> np.array:
         """
         TBD
-        
+
         """
         samples = multivariate_normal(mean, cov, size=m)
         return samples
 
-    def rand_population_uniform(self: 'PopulationMethods', m: int, a: list, b: list) -> np.array:
+    def rand_population_uniform(self: 'GeneticAlgorithm', m: int, a: list, b: list) -> np.array:
         d = len(a)
-        _samples = ([a[i] + np.random.random(d) * (b[i] - a[i]) for i in range(d) for j in range(m)])
+        _samples = ([a[i] + np.random.random(d) * (b[i] - a[i])
+                    for i in range(d) for j in range(m)])
         samples = np.array([arr.tolist() for arr in _samples])
         # samples = np.array(_).flatten().tolist()
         return samples
 
     def rand_population_cauchy(self, m: int, mean: list, scale: list) -> np.array:
         n = len(mean)
-        sample = np.array([[standard_cauchy() * scale[j] + mean[j] for j in range(n)] for i in range(m)])
+        sample = np.array([[standard_cauchy() * scale[j] + mean[j]
+                          for j in range(n)] for i in range(m)])
         return sample
 
-    def genetic_algorithm(f: Callable, select: Callable, crossover: Callable, mutate: Callable, population: list, k_max: int, S: int, C: float, M: float) -> list:
+    def genetic_algorithm(f: Callable, select: Callable, crossover: Callable, mutate: Callable, population: list, k_max: int) -> list:
         for k in range(k_max):
-            parents = select(S, [f(p) for p in population])
-            children = [crossover(C, population[p[0]], population[p[1]]) for p in parents]
-            population = [mutate(M, child) for child in children]
-        new_poulation = population[min(range(len(population)), key=lambda i: f(population[i]))]
+            parents = select(y=f(population))
+            children = [crossover(parent_1=population[p[0]], parent_2=population[p[1]]) for p in parents]
+            population = [mutate(child, r=0.5) for child in children]
+        # new_poulation = population[min(range(len(population)), key=lambda i: f(population[i]))]
+        new_poulation = population[np.argmin(f(population))]
         return new_poulation
 
     def generate_rand_bit_population(self, m: int) -> list:
         rand_bit_pop = [getrandbits(1) for i in range(m)]
         return rand_bit_pop
-    
-
